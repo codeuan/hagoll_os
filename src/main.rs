78e808e13c2257d39e66a7b32979edf867e9f7ff
MaxwellML <cmdp7@outlook.com>
2026-08-29 17:21:32 +0100

    Added bootable image. Added "Hello world" message display.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 #![no_std]
 #![no_main]
-
+//run with qemu-system-x86_64 -drive format=raw,file=target/x86_64-blog_os/debug/bootimage-blog_os.bin
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
